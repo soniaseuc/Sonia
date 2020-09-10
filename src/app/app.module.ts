99c7beffbe32e40e423ec9362831af6c3f435f81
Sonia Seuc <sonia.seuc@gmail.com>
2020-09-09 21:46:35 -0500

    with material menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatMenuModule, MatMenuTrigger} from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppComponent } from './app.component';
 import { AboutComponent } from './components/about/about.component';
@@ -10,6 +12,7 @@ import { ShowemailComponent } from './components/showemail/showemail.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { BurgerComponent } from './burger/burger.component';
 import { MenuComponent } from './menu/menu.component';
+import { MenuButtonComponent } from './components/menu-button/menu-button.component';
 
 @NgModule({
   declarations: [
@@ -19,11 +22,15 @@ import { MenuComponent } from './menu/menu.component';
     ShowemailComponent,
     NavbarComponent,
     BurgerComponent,
-    MenuComponent
+    MenuComponent,
+    MenuButtonComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatMenuModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/menu-button/menu-button.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import {MatMenuModule, MatMenuTrigger} from '@angular/material/menu';
+
+/**
+ * @title Menu with icons
+ */
+
+@Component({
+  selector: 'app-menu-button',
+  templateUrl: './menu-button.component.html',
+  styleUrls: ['./menu-button.component.scss']
+})
+export class MenuButtonComponent implements OnInit {
+  @ViewChild(MatMenuTrigger) trigger: MatMenuTrigger;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  someMethod(): void {
+    this.trigger.openMenu();
+  }
+}
+
+
+
+// @Component({
+//   selector: 'menu-icons-example',
+//   templateUrl: 'menu-icons-example.html',
+// })
+// export class MenuIconsExample {}
